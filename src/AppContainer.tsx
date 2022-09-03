3b7a2da7f2939603c83440784c1c83075b256404
Eugen Guriev <gto200313@gmail.com>
2022-09-03 17:24:01 +0300

    changes for gh-pages

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import store from "./redux/store";
 
 export const AppContainer: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
